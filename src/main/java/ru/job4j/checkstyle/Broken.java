0430d7146b4abee9fb18dc0eb0bac6b12c9c3cbb
KEWNRU <n0kky21rus@gmail.com>
2023-02-15 19:16:55 +0300

    Правила Checkstyle [#504981 #391919]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -15,13 +15,17 @@ public class Broken {
             System.out.println(obj);
         }
 
-        void method ( int a, int b, int c, int d, int e, int f, int g){
-        }
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+        int g;
+        int h;
 
-        private String name;
+        String name;
 
-        Broken() {
-        }
+        new Broken();
 
     }
 }
