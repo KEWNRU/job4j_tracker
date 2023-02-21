0181eff89861aefbbc6028c4f6adaceab89e1371
KEWNRU <n0kky21rus@gmail.com>
2023-02-21 15:17:51 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("Сожрали бублик");
+        } else {
+            System.out.println("Убежал бублик");
+        }
+    }
+}
