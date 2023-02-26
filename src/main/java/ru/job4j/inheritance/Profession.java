56d3ff15221af1463e31c2513d01a29279bd01f6
KEWNRU <n0kky21rus@gmail.com>
2023-02-26 20:55:26 +0300

    3. Вызов конструктора родителя super(...) [#6837]

---
+++ src/main/java/ru/job4j/inheritance/Profession.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class Profession {
+    private boolean degree;
+    private int experience;
+
+    public Profession(int experience, boolean degree) {
+        this.experience = experience;
+        this.degree = degree;
+    }
+}
