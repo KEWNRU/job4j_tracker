8f79f7bffc56ab7db959bcd81231860116d6fecc
KEWNRU <n0kky21rus@gmail.com>
2023-03-08 15:04:44 +0300

    4. Сравнение моделей. Метод equals [#174893]

--- src/test/java/ru/job4j/pojo/LicenseTest.java
+++ src/test/java/ru/job4j/pojo/LicenseTest.java
@@ -13,4 +13,13 @@ public class LicenseTest {
         second.setCode("audio");
         assertThat(first).isEqualTo(second);
     }
+
+    @Test
+    public void eqName1() {
+        License first = new License();
+        first.setCode("1337");
+        License second = new License();
+        second.setCode("1337");
+        assertThat(first).isEqualTo(second);
+    }
 }
