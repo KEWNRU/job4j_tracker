bca4a110f241cd9e308d0bb39bbab59e8ef46240
KEWNRU <n0kky21rus@gmail.com>
2023-04-13 10:32:24 +0300

    2. Идентичные тексты [#212655 #409206]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -14,6 +14,7 @@ public class UniqueText {
         for (String j : text) {
             if (!check.contains(j)) {
                 rsl = false;
+                break;
             }
         }
         return rsl;
