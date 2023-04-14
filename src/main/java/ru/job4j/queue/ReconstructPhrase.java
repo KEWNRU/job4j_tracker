81533c8a8e9439870ca315a2728881c1dd4d0331
KEWNRU <n0kky21rus@gmail.com>
2023-04-14 15:35:58 +0300

    2. Интерфейс Deque [#504943]

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,41 @@
+package ru.job4j.queue;
+
+import java.util.Deque;
+import java.util.Iterator;
+
+public class ReconstructPhrase {
+
+    private final Deque<Character> descendingElements;
+
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder stringBuilder = new StringBuilder();
+        int elements = evenElements.size();
+        for (int i = 0; i < elements; i++) {
+            Character element = evenElements.poll();
+            if (i % 2 == 0) {
+                stringBuilder.append(element);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private String getDescendingElements() {
+        Iterator<Character> iterator = descendingElements.descendingIterator();
+        StringBuilder stringBuilder = new StringBuilder();
+        while (iterator.hasNext()) {
+            stringBuilder.append(iterator.next());
+        }
+        return stringBuilder.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
