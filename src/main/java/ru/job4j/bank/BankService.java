93e585fb32fb79db78533cf3b2593a0ded371f84
KEWNRU <n0kky21rus@gmail.com>
2023-04-19 16:20:20 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,65 @@
+package ru.job4j.bank;
+import ru.job4j.bank.Account;
+import ru.job4j.bank.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+            users.put(user, new ArrayList<>());
+    }
+
+    public boolean deleteUser(String passport) {
+        return users.remove(new User(passport, "")) != null;
+    }
+
+    public void addAccount(String passport, Account account) {
+        List<Account> accounts = users.get(findByPassport(passport));
+        if (!accounts.contains(account)) {
+            accounts.add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user :users.keySet()) {
+            if (passport.equals(user.getPassport())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        List<Account> accounts = users.get(findByPassport(passport));
+        if (accounts != null) {
+            for (Account account : accounts) {
+                if (requisite.equals(account.getRequisite())) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account src = findByRequisite(srcPassport, srcRequisite);
+        Account dest = findByRequisite(destPassport, destRequisite);
+        if (src != null && dest != null && src.getBalance() >= amount) {
+            rsl = true;
+            src.setBalance(src.getBalance() - amount);
+            dest.setBalance(dest.getBalance() + amount);
+        }
+        return rsl;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
