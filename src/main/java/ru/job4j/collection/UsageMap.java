7d5dc2c61269a7ebc2b8518d4db76ee2ab7a6593
KEWNRU <n0kky21rus@gmail.com>
2023-04-19 10:16:09 +0300

    0. Map, HashMap [#212652 #410533]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("n0kky21rus@gmail.com", "Petya");
         map.put("n0kky21rus@gmail.com", "Ivan Popov");
+        map.put("123@gmail.com", "Igor");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
