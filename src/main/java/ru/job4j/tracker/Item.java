1becbde85856621f5fe3c9ca43a4e54398294b57
KEWNRU <n0kky21rus@gmail.com>
2023-04-22 10:18:19 +0300

    0. Сортировка [#10096 #411379]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -52,10 +52,15 @@ public class Item {
                 + ", name='" + name + '\''
                 + '}';
     }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Item item = (Item) o;
         return id == item.id && Objects.equals(name, item.name) && Objects.equals(created, item.created);
     }
