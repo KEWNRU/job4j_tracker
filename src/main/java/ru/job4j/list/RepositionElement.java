c7dbf865e16077c7bfb4536424ae3ab477fe3d27
KEWNRU <n0kky21rus@gmail.com>
2023-05-12 14:30:29 +0300

    2.2. Функциональный интерфейс [#247497]

---
+++ src/main/java/ru/job4j/list/RepositionElement.java
@@ -0,0 +1,9 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+public class RepositionElement {
+    public static List<String> changePosition(List<String> list, int index) {
+        return list;
+    }
+}
