467210a2e55e9c0520de2f85f19f1fb0af0f25b9
KEWNRU <n0kky21rus@gmail.com>
2023-05-29 07:55:29 +0300

    7. Функции высшего порядка [#24255 #419282]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -7,6 +7,16 @@ import java.util.ArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PhoneDictionaryTest {
+
+    @Test
+    public void whenNull() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Ivan");
+        assertThat(persons.isEmpty()).isTrue();
+    }
     @Test
     public void whenFindByName() {
         PhoneDictionary phones = new PhoneDictionary();
