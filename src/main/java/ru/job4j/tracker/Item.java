2f3764841d70c3de3f8b1cd950de7318326c0c5f
KEWNRU <n0kky21rus@gmail.com>
2024-12-13 13:10:31 +0300

    1. Lombok. Настройка IDEA [#504888]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,12 +7,10 @@ import java.time.format.DateTimeFormatter;
 
 @Data
 public class Item {
-
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-
-    private LocalDateTime created = LocalDateTime.now();
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now().withNano(0);
 
     public Item(int id, String name) {
         this.id = id;
@@ -22,15 +20,4 @@ public class Item {
     public Item(String name) {
         this.name = name;
     }
-
-    @Override
-    public String toString() {
-        return "Item{"
-                + "created=" + created.format(FORMATTER)
-                + ", id=" + id
-                + ", name='" + name + '\''
-                + '}';
-    }
-
 }
-
