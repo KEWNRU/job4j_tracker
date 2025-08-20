1393600a50ee354f2f02f890644557a6caa03827
KEWNRU <n0kky21rus@gmail.com>
2025-08-20 22:00:18 +0300

    1. Интеграционное тестирование с Hibernate [#6875]

---
+++ src/test/java/ru/job4j/tracker/HbmTrackerTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HbmTrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item = new Item();
+            item.setName("test1");
+            tracker.add(item);
+            Item result = tracker.findById(item.getId());
+            assertThat(result.getName()).isEqualTo(item.getName());
+        }
+    }
+
+    @Test
+    public void whenItemFindById() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item = new Item();
+            item.setName("test1");
+            tracker.add(item);
+            Item rsl = tracker.findById(item.getId());
+            assertThat(rsl.getName()).isEqualTo("test1");
+        }
+    }
+
+    @Test
+    public void whenDeleteItemThenTracker() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item = new Item();
+            item.setName("test1");
+            tracker.add(item);
+            int id = item.getId();
+            tracker.delete(id);
+            assertThat(tracker.findById(id)).isNull();
+        }
+    }
+}
